4772e1e2b7fe0853048b8646e9dc7ef182f2e8b6
Florian Klüber <Florian.Klueber@css.de>
2022-04-30 12:23:17 +0200

    qantisegrayscale not complete

--- src/03/grayscale.ts
+++ src/03/grayscale.ts
@@ -14,20 +14,17 @@ export function grayscale(x: number, y: number, source: Uint8ClampedArray, targe
     // TODO: Convert the pixel at position (x, y) in the source array from RGB to XYZ.
     // TODO: Set the RGBA values in the target array according to the Y component of the source pixel in XYZ space.
     let position = (y*(width*4))+(x*4);
-        
+    
     let r=source[position];
     let g=source[position+1];
     let b=source[position+2];
     let a=source[position+3];
 
-
-
-
+    //umrechnung von rgb nach xyz
     const X =  0.4124564*r+0.3575761*g+0.1804375*b;
     const Y =  0.2126729*r+0.7151522*g+0.0721750*b;
     const Z =  0.0193339*r+0.1191920*g+0.9503041*b;
  
-    
     target.set([0, 0, 0, 255-Y], position);
 
 }
--- src/03/quantisegrayscale.ts
+++ src/03/quantisegrayscale.ts
@@ -12,6 +12,25 @@
 export function quantisegrayscale(x: number, y: number, source: Uint8ClampedArray, target: Uint8ClampedArray, width: number, height: number) {
 
     // TODO: Convert the pixel at position (x, y) in the source array from RGB to XYZ. Limit the 
+    let position = (y*(width*4))+(x*4);
+    
+    let r=source[position];
+    let g=source[position+1];
+    let b=source[position+2];
+    let a=source[position+3];
+
+    //umrechnung von rgb nach xyz
+    const X =  0.4124564*r+0.3575761*g+0.1804375*b;
+    const Y =  0.2126729*r+0.7151522*g+0.0721750*b;
+    const Z =  0.0193339*r+0.1191920*g+0.9503041*b;
+
     // TODO: Limit the brightness to the set of 4 different values 0, 85, 170, 255.
+    // let Y_quant =(Math.floor(Math.pow(255, 1/Y)*Math.pow(2,4))%4)*85;
+    let Y_quant=Math.round(Y/85)*85
+    console.log(Y_quant);
+    
     // TODO: Set the RGBA values in the target array to this brightness.
+    
+    target.set([0, 0, 0,255-Y_quant], position);
+
 }
