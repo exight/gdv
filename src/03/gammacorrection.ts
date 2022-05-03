f6f19c9f9b66b5fad0c582fa6f065eede2ff00e8
Florian Klüber <Florian.Klueber@css.de>
2022-05-03 15:03:10 +0200

    gammacorrection fertig

--- src/03/gammacorrection.ts
+++ src/03/gammacorrection.ts
@@ -15,4 +15,21 @@ export function gammaAdjust(gamma: number, source: Uint8ClampedArray,
                             width: number, height: number) {
 
     // TODO: Perform a gamma correction with the given gamma value on the current pixel at position (x, y) in the source array, and store the result in the dest array.
+    let position = (y*(width*4))+(x*4);
+    
+    let r=source[position];
+    let g=source[position+1];
+    let b=source[position+2];
+    let a=source[position+3];
+    // console.log(gamma);
+
+    dest.set([setGamma(r,gamma), setGamma(g,gamma), setGamma(b,gamma), setGamma(a,gamma)], position);
+
 }
+
+function setGamma(value:number,gamma:number){
+    // console.log(value/255);
+    
+    return Math.pow(value/255,1/gamma)*Math.pow(2, 8);
+}
+    
